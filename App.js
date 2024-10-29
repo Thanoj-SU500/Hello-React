83468c817d6c41305efd5535f11c724c34856a28
thanoj <thanoj@knowledgelens.com>
2024-10-29 23:00:32 +0530

    Ep-03-01

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement("div", {id: 'parent'}, 
-    [React.createElement('div', {id: 'child1'},
-        [React.createElement('h1', {}, "Im H1 tag "), 
-            React.createElement('h2', {}, "Im H2 tag ")])
-    ],
-    React.createElement('div', {id: 'child1'},
-        [React.createElement('h1', {}, "Im H1 tag "), 
-            React.createElement('h2', {}, "Im H2 tag ")]
-))
+const Heading = () => (
+<h1>
+    Hi From jsx heading
+</h1>
+)
+const HeaderComponent = () => (
+    <div id="container">
+        <Heading />
+     <h2>This is A header 2 from the component</h2>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent);
+root.render(<HeaderComponent />);
